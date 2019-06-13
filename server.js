a30d8abb2df736d84f7eb091840cb7615a4bf535
kfkn749 <jose.arcila@astrazeneca.com>
2019-06-13 09:45:42 -0500

    first webpage

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
+/*
 app.get('/', (request, response) => {
 response.send('Hello World')
 })
+*/
+
 app.listen(9000)
